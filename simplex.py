3a92ced6ce267a8491f594bf3de6a4c04aae832c
Atul <atuliraz1305@gmail.com>
2023-05-10 17:21:37 +0530

    Added Gomory Cut and Fractions

--- simplex.py
+++ simplex.py
@@ -1,4 +1,5 @@
 import numpy as np
+from fractions import Fraction as F
 def chk_simp_opt(tab):
     j = (np.argmin(tab[0, 1:]))+1
     if(tab[0][j])>=0:
@@ -15,7 +16,18 @@ def chk_dual_opt(tab):
     
 def add_gomory(tab, i):
     #Add gomory cut of i source to tableau tab
-    pass
+    m1,n1 = np.shape(tab)
+    tab = np.vstack((tab, np.zeros((1,n1))))
+    tab = np.hstack((tab, np.zeros((m1+1,1))))
+    temp = tab[i].copy()
+    for j in range(n1):
+        temp[j] = np.floor(float(temp[j]))
+    
+    tab[-1] = fractionise(temp-tab[i])
+    tab[-1][-1] =1
+    tab[:,-1] = fractionise(tab[:,-1])
+
+    return tab
 
 def dual_simplex_iter(M, tab):
     m,n = np.shape(tab)
@@ -32,12 +44,34 @@ def dual_simplex_iter(M, tab):
     tab[j,:] = tab[j,:]/tab[j,i]
     for z in range(m):
         if z!=j:
-            tab[z,:] = tab[z,:] - (tab[j,:]*tab[z,i]
+            tab[z,:] = tab[z,:] - (tab[j,:]*tab[z,i])
     return M,tab
     
     
     #Perform dual simplex iteration on tableau tab
     pass
+
+def load(filename):
+    with open('test.txt', 'r') as fd:
+        reader = fd.readlines()
+        n,m = map(int, reader[0].rstrip('\n').split(" "))
+        b = list(map(F, reader[1].rstrip('\n').split(" ")))
+        c = list(map(F, reader[2].rstrip('\n').split(" ")))
+        a = []
+        for i in range(m):
+            a.append(list(map(F, reader[3+i].split(" "))))
+    return n,m,b,c,a
+
+def fractionise(tab):
+    l = np.shape(tab)
+    if(len(l)==1):
+        for i in range(l[0]):
+            tab[i] = F(tab[i])
+    else:
+        for i in range(l[0]):
+            for j in range(l[1]):
+                tab[i][j] = F(tab[i][j])
+    return tab
 
 def simplex_iter(M, tab):
     m,n = np.shape(tab)
@@ -57,17 +91,6 @@ def simplex_iter(M, tab):
             tab[k,:] = tab[k,:] - (tab[i, :])*tab[k][j]
 
     return M,tab
-
-def load(filename):
-    with open('test.txt', 'r') as fd:
-        reader = fd.readlines()
-        n,m = map(int, reader[0].rstrip('\n').split(" "))
-        b = list(map(int, reader[1].rstrip('\n').split(" ")))
-        c = list(map(int, reader[2].rstrip('\n').split(" ")))
-        a = []
-        for i in range(m):
-            a.append(list(map(int, reader[3+i].split(" "))))
-    return n,m,b,c,a
 
 def simplex(n,m,tab, c):
     #Phase 1
@@ -99,12 +122,19 @@ def simplex(n,m,tab, c):
     for j in range(1,len(tab[0])):
         tab[0][j] = c[j-1] - np.dot([c[_-1] for _ in M], tab[1:,j])
 
+    tab[0] = fractionise(tab[0])
     while(not chk_simp_opt(tab)):
         M, tab = simplex_iter(M, tab)
 
     return M, tab
 
-    
+def solution(tab, M, n):
+    x = [0 for i in range(n)]
+    for i in range(len(M)):
+        if(M[i]<=n):
+            x[M[i]-1] = int(tab[i+1][0])
+    return x
+
     
 def gomory(filename):
     n,m,b,c,a = load(filename)  
@@ -119,15 +149,17 @@ def gomory(filename):
         flag=0
         frac = 0
         #Find fractional basic variable
-        for i in M:
-            if (tab[0][i] - int(tab[0][i]))!=0:
-                flag=1
+        for i in range(1, len(M)+1):
+            if(tab[i][0]%1!=0):
                 frac = i
+                flag=1
                 break
         if(flag==0):
-            return tab[0][M]
+            return solution(tab, M, n)
         
         tab = add_gomory(tab, frac)
+        M.append(len(tab[0]))
+
         while(not chk_dual_opt(tab)):
             M, tab = dual_simplex_iter(M, tab)
 
@@ -163,6 +195,8 @@ def create_tableau(n,m,b,c,a):
     b = b1
 
     tab = np.hstack((b, tab))
+
+    fractionise(tab)
     return tab, c
 
 print(gomory('test.txt'))
