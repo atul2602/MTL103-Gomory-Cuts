ffeaeac2d5f1f0d8b64ce5396696b92d71264c29
Atul <atuliraz1305@gmail.com>
2023-05-10 15:39:54 +0530

    Completed Two Phase simplex implementation

--- simplex.py
+++ simplex.py
@@ -43,26 +43,60 @@ def simplex_iter(M, tab):
 def load(filename):
     with open('test.txt', 'r') as fd:
         reader = fd.readlines()
-        n,m = map(int, reader[0].split(" "))
-        b = list(map(int, reader[1].split(" ")))
-        c = list(map(int, reader[2].split(" ")))
+        n,m = map(int, reader[0].rstrip('\n').split(" "))
+        b = list(map(int, reader[1].rstrip('\n').split(" ")))
+        c = list(map(int, reader[2].rstrip('\n').split(" ")))
         a = []
         for i in range(m):
             a.append(list(map(int, reader[3+i].split(" "))))
     return n,m,b,c,a
 
-def gomory(filename):
-    n,m,b,c,a = load(filename)  
-    tab = create_tableau(n,m,b,c,a)
+def simplex(n,m,tab, c):
+    #Phase 1
+    M = [i for i in range(n+m+1,n+2*m+1)]
+    while(not chk_simp_opt(tab)):
+        M, tab = simplex_iter(M, tab) 
 
-    # M is list of basic indices, in 1 indexed form
-    M = [i for i in range(n,n+m)]
+    tab = np.array(tab)
+    tbd=[]
+    # for i in range(n+m+1, n+2*m+1):
+    #     if(i not in M):
+    #         tbd.append(i)
+    
+    tab = np.delete(tab, [j for j in range(n+m+1, n+2*m+1)], axis = 1)
+
+    tbd = []
+    for i in range(len(M)):
+        if(M[i]>n+m):
+            tbd.append(i+1)
+    
+    M1 = []
+    for i in range(len(M)):
+        if(i+1 not in tbd):
+            M1.append(M[i])
+
+    M = M1
+
+    tab = np.delete(tab, tbd, axis = 0)
+    for j in range(1,len(tab[0])):
+        tab[0][j] = c[j-1] - np.dot([c[_-1] for _ in M], tab[1:,j])
 
     while(not chk_simp_opt(tab)):
         M, tab = simplex_iter(M, tab)
 
-    return tab
+    return M, tab
+
     
+    
+def gomory(filename):
+    n,m,b,c,a = load(filename)  
+    tab, c = create_tableau(n,m,b,c,a)
+
+    # M is list of basic indices, in 1 indexed form
+    M = [i for i in range(n+m,n+2*m)]
+
+    M, tab = simplex(n, m, tab, c)
+
     while(True):
         flag=0
         frac = 0
@@ -75,21 +109,34 @@ def gomory(filename):
         if(flag==0):
             return tab[0][M]
         
-        tab = add_gomory(tab, i)
+        tab = add_gomory(tab, frac)
         while(not chk_dual_opt(tab)):
             M, tab = dual_simplex_iter(M, tab)
 
 
     # Take input from file "filename"
-    #return x # an array of n integers
+    # return x # an array of n integers
 
 def create_tableau(n,m,b,c,a):
-    c = -1*np.hstack((c, np.zeros(m)))
+    c = np.array(c)
+    c = -1*np.hstack((c, [0 for i in range(m)]))   #like a minimisation problem
+    c1 = np.zeros((1, n+2*m))     #for auxiliary problem
+    
     tab = a
-    tab = np.hstack((tab, np.zeros((m,m))))
+    tab = np.hstack((tab, np.zeros((m,2*m))))
+
     for i in range(m):
         tab[i][i+n] = 1
-    tab = np.vstack((c, tab))
+        if(b[i]<0):
+            tab[i] = -1*tab[i]
+            b[i] = -1*b[i]
+        tab[i][i+n+m] = 1
+    
+    #Configuring reduced costs for x, and s
+    c1[0][:n+m] = -1*np.sum(tab[:, :n+m], axis = 0)
+    
+            
+    tab = np.vstack((c1, tab))
     b.insert(0,0)
 
     b1 = []
@@ -98,7 +145,7 @@ def create_tableau(n,m,b,c,a):
     b = b1
 
     tab = np.hstack((b, tab))
-    return tab
+    return tab, c
 
 print(gomory('test.txt'))
 
